809025e567f6c53d49e38d24a5fd0feeac85d385
Matt Dailey <mdailey@palantir.com>
2016-05-28 14:18:52 -0700

    reset late count every day

--- app/src/main/java/com/reactiverobot/latecounter/LateCounterPrefs.java
+++ app/src/main/java/com/reactiverobot/latecounter/LateCounterPrefs.java
@@ -3,9 +3,14 @@ package com.reactiverobot.latecounter;
 import android.content.Context;
 import android.content.SharedPreferences;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class LateCounterPrefs {
 
-    public static final String TODAYS_LATE_COUNT = "todays_late_count";
+    public static final String TODAYS_LATE_COUNT_PREF = "todays_late_count";
+    private static final String TODAY_STRING_PREF = "today_string";
+
     private final Context context;
 
     public LateCounterPrefs(Context context) {
@@ -17,15 +22,24 @@ public class LateCounterPrefs {
     }
 
     public int getTodaysLateCount() {
-        return getPrefs().getInt(TODAYS_LATE_COUNT, 0);
+        String todayString = new SimpleDateFormat("MM/dd/yyyy").format(new Date()).toString();
+
+        String lastRecordedIncrement = getPrefs().getString(TODAY_STRING_PREF, "unknown");
+
+        if (todayString.equals(lastRecordedIncrement)) {
+            return getPrefs().getInt(TODAYS_LATE_COUNT_PREF, 0);
+        } else {
+            getPrefs().edit()
+                    .putString(TODAY_STRING_PREF, todayString)
+                    .putInt(TODAYS_LATE_COUNT_PREF, 0)
+                    .apply();
+            return 0;
+        }
     }
 
     public void incrementLateCount() {
         int lateCount = getTodaysLateCount();
-        getPrefs().edit().putInt(TODAYS_LATE_COUNT, lateCount + 1).apply();
+        getPrefs().edit().putInt(TODAYS_LATE_COUNT_PREF, lateCount + 1).apply();
     }
 
-    public void clearLateCount() {
-        getPrefs().edit().remove(TODAYS_LATE_COUNT);
-    }
 }
