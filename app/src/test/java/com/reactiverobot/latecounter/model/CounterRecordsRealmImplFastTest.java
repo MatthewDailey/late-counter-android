1cdf15a11c5677777755871d7f413f6c2c769292
Matt Dailey <mdailey@palantir.com>
2016-06-11 21:28:44 -0700

    pull realm mocking out into its own class

--- app/src/test/java/com/reactiverobot/latecounter/model/CounterRecordsRealmImplFastTest.java
+++ app/src/test/java/com/reactiverobot/latecounter/model/CounterRecordsRealmImplFastTest.java
@@ -2,15 +2,7 @@ package com.reactiverobot.latecounter.model;
 
 
 import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
-import org.mockito.invocation.InvocationOnMock;
-import org.mockito.stubbing.Answer;
-import org.powermock.api.mockito.PowerMockito;
-import org.powermock.core.classloader.annotations.PowerMockIgnore;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.rule.PowerMockRule;
-import org.roboguice.shaded.goole.common.base.Suppliers;
 
 import java.util.Date;
 
@@ -19,28 +11,15 @@ import io.realm.Realm;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
-import static org.powermock.api.mockito.PowerMockito.doAnswer;
 import static org.powermock.api.mockito.PowerMockito.when;
 
-@PowerMockIgnore({"org.mockito.*", "org.robolectric.*", "android.*"})
-@PrepareForTest({Realm.class})
-public class CounterRecordsRealmImplFastTest {
-
-    @Rule
-    public PowerMockRule powerMock = new PowerMockRule();
-
-    private RealmSupplier realmSupplier;
-    private Realm realm;
+public class CounterRecordsRealmImplFastTest extends RealmMockingTest {
 
     private CounterType counterType;
 
     @Before
     public void setup() {
-        realm = PowerMockito.mock(Realm.class);
         counterType = mock(CounterType.class);
-        realmSupplier = new RealmSupplierImpl(Suppliers.ofInstance(realm));
-
-        initializeRealmMockToExecuteTransactions();
     }
 
     @Test
@@ -56,13 +35,4 @@ public class CounterRecordsRealmImplFastTest {
         verify(realm).close();
     }
 
-    private void initializeRealmMockToExecuteTransactions() {
-        doAnswer(new Answer() {
-            @Override
-            public Object answer(InvocationOnMock invocation) throws Throwable {
-                ((Realm.Transaction) invocation.getArguments()[0]).execute(realm);
-                return null;
-            }
-        }).when(realm).executeTransaction(any(Realm.Transaction.class));
-    }
 }
---
+++ app/src/test/java/com/reactiverobot/latecounter/model/RealmMockingTest.java
@@ -0,0 +1,46 @@
+package com.reactiverobot.latecounter.model;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PowerMockIgnore;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.rule.PowerMockRule;
+import org.roboguice.shaded.goole.common.base.Suppliers;
+
+import io.realm.Realm;
+
+import static org.mockito.Matchers.any;
+import static org.powermock.api.mockito.PowerMockito.doAnswer;
+
+@PowerMockIgnore({"org.mockito.*", "org.robolectric.*", "android.*"})
+@PrepareForTest({Realm.class})
+public abstract class RealmMockingTest {
+
+    @Rule
+    public PowerMockRule powerMock = new PowerMockRule();
+
+    public RealmSupplier realmSupplier;
+    public Realm realm;
+
+    @Before
+    public void setupRealmMock() {
+        realm = PowerMockito.mock(Realm.class);
+
+        realmSupplier = new RealmSupplierImpl(Suppliers.ofInstance(realm));
+
+        initializeRealmMockToExecuteTransactions();
+    }
+
+    private void initializeRealmMockToExecuteTransactions() {
+        doAnswer(new Answer() {
+            @Override
+            public Object answer(InvocationOnMock invocation) throws Throwable {
+                ((Realm.Transaction) invocation.getArguments()[0]).execute(realm);
+                return null;
+            }
+        }).when(realm).executeTransaction(any(Realm.Transaction.class));
+    }
+}
