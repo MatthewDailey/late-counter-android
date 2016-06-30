022d5d5c9b7dc263bf1c934bfb7b169948c63ba3
Matthew Dailey <matthew.j.dailey@gmail.com>
2016-06-30 15:37:30 -0700

    add layout to CreateCounterTypeActivity

--- app/src/main/java/com/reactiverobot/latecounter/activity/CreateCounterTypeActivity.java
+++ app/src/main/java/com/reactiverobot/latecounter/activity/CreateCounterTypeActivity.java
@@ -1,6 +1,9 @@
 package com.reactiverobot.latecounter.activity;
 
+import android.os.Bundle;
+
 import com.google.inject.Inject;
+import com.reactiverobot.latecounter.R;
 import com.reactiverobot.latecounter.model.CounterTypes;
 
 import roboguice.activity.RoboActivity;
@@ -9,4 +12,11 @@ public class CreateCounterTypeActivity extends RoboActivity {
 
     @Inject
     CounterTypes counterTypes;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.create_counter_type_layout);
+    }
 }
--- app/src/test/java/com/reactiverobot/latecounter/activity/CreateCounterTypeActivityTest.java
+++ app/src/test/java/com/reactiverobot/latecounter/activity/CreateCounterTypeActivityTest.java
@@ -1,14 +1,21 @@
 package com.reactiverobot.latecounter.activity;
 
+import android.widget.Button;
+import android.widget.EditText;
+
 import com.google.inject.Module;
 import com.reactiverobot.latecounter.AbstractRoboTest;
+import com.reactiverobot.latecounter.R;
 import com.reactiverobot.latecounter.model.MockModelModule;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.robolectric.Robolectric;
 
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
 
 public class CreateCounterTypeActivityTest extends AbstractRoboTest {
 
@@ -20,14 +27,20 @@ public class CreateCounterTypeActivityTest extends AbstractRoboTest {
         return new Module[]{mockModelModule};
     }
 
+    private CreateCounterTypeActivity activity;
+
     @Override
     protected void setup() {
-
+        activity = Robolectric.setupActivity(CreateCounterTypeActivity.class);
     }
 
     @Test
     public void testCanStartCreateCounterTypeActivity() {
-        assertNotNull(Robolectric.setupActivity(CreateCounterTypeActivity.class));
+        assertNotNull(activity);
+
+        assertThat(activity.findViewById(R.id.create_counter_text), is(instanceOf(EditText.class)));
+        assertThat(activity.findViewById(R.id.create_counter_submit), is(instanceOf(Button.class)));
+        assertThat(activity.findViewById(R.id.create_counter_cancel), is(instanceOf(Button.class)));
     }
 
 }
