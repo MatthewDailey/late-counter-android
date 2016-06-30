12910932cafb81eb10da5a4c4ba9eb1f3b6e0812
Matthew Dailey <matthew.j.dailey@gmail.com>
2016-06-30 00:40:21 -0700

    introduce CounterRecordTest

---
+++ app/src/androidTest/java/com/reactiverobot/latecounter/model/CounterRecordTest.java
@@ -0,0 +1,29 @@
+package com.reactiverobot.latecounter.model;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class CounterRecordTest {
+
+    @Rule
+    public RealmSupplierRule realm = new RealmSupplierRule();
+
+    private CounterRecords counterRecords;
+
+    @Before
+    public void setupCounterTypes() {
+        counterRecords = new CounterRecordsRealmImpl(realm.getRealmSupplier());
+    }
+
+    @Test
+    public void testGetTodaysCountStartsAtZero() {
+        CounterType counterType = CounterType.withDescription("test-desc");
+        CounterRecord todaysCount = counterRecords.getTodaysCount(counterType);
+        assertThat(todaysCount.getCount(), is(equalTo(0)));
+    }
+}
