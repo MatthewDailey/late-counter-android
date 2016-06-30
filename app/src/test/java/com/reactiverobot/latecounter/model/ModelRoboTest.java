d4101f323a063b049253ec50f2273754189d6866
Matthew Dailey <matthew.j.dailey@gmail.com>
2016-06-29 23:13:17 -0700

    add basic CounterWidgetRoboTest

--- app/src/test/java/com/reactiverobot/latecounter/model/ModelRoboTest.java
+++ app/src/test/java/com/reactiverobot/latecounter/model/ModelRoboTest.java
@@ -10,7 +10,7 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertThat;
 
-public class ModelRoboTest extends AbstractRoboTest{
+public class ModelRoboTest extends AbstractRoboTest {
 
     @Override
     protected void setup() {
---
+++ app/src/test/java/com/reactiverobot/latecounter/widget/CounterWidgetRoboTest.java
@@ -0,0 +1,43 @@
+package com.reactiverobot.latecounter.widget;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+
+import com.google.inject.Module;
+import com.reactiverobot.latecounter.AbstractRoboTest;
+import com.reactiverobot.latecounter.R;
+import com.reactiverobot.latecounter.model.ModelModule;
+import com.reactiverobot.latecounter.prefs.PrefsModule;
+
+import org.junit.Test;
+import org.robolectric.Shadows;
+import org.robolectric.shadows.ShadowAppWidgetManager;
+
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class CounterWidgetRoboTest extends AbstractRoboTest {
+    @Override
+    protected void setup() {
+
+    }
+
+    @Override
+    protected Module[] getModules() {
+        return new Module[]{new ModelModule(), new PrefsModule()};
+    }
+
+    @Test
+    public void testInstantiateWidget() {
+        ShadowAppWidgetManager shadowAppWidgetManager = Shadows
+                .shadowOf(AppWidgetManager.getInstance(context));
+        int widgetId = shadowAppWidgetManager
+                .createWidget(CounterWidget.class, R.layout.counter_widget);
+
+        AppWidgetProvider appWidgetProvider =
+                shadowAppWidgetManager.getAppWidgetProviderFor(widgetId);
+        assertThat(appWidgetProvider, is(instanceOf(CounterWidget.class)));
+    }
+
+}
